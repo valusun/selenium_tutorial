0f85b1a202de8c7b333bd56d8ecfa0db0f979345
valusun <chobicoco0327@gmail.com>
2022-12-24 18:31:41 +0900

    update/構成変えた

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from dataclasses import dataclass, field
-from time import sleep
+from dataclasses import InitVar, dataclass, field
 
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
@@ -11,20 +10,26 @@ from dl_webdriver import DownloadWebDriver
 
 
 @dataclass
-class DriverOperator:
+class WebDriverWindow:
     driver: WebDriver
     wait: WebDriverWait = field(init=False)
+    time_out: InitVar[float] = 10
 
-    def __post_init__(self):
-        self.wait = WebDriverWait(self.driver, timeout=10)
+    def __post_init__(self, time_out):
+        self.wait = WebDriverWait(self.driver, timeout=time_out)
+
+
+@dataclass
+class ChromeWindow:
+    web: WebDriverWindow
 
     def Start(self, url: str):
         """受け取ったURL先にアクセスする"""
-        self.driver.get(url)
+        self.web.driver.get(url)
 
     def Quit(self):
         """WebDriverを閉じる"""
-        self.driver.quit()
+        self.web.driver.quit()
 
     def _InputField(self, elm: WebElement, value: str):
         """入力バーに要素を入力する"""
@@ -33,33 +38,32 @@ class DriverOperator:
 
     def InputFieldByXPATH(self, xpath: str, value: str):
         """xpathを指定して入力バーに入力する"""
-        self.wait.until(EC.visibility_of_element_located((By.XPATH, xpath)))
-        elm = self.driver.find_element(By.XPATH, xpath)
+        self.web.wait.until(EC.visibility_of_element_located((By.XPATH, xpath)))
+        elm = self.web.driver.find_element(By.XPATH, xpath)
         self._InputField(elm, value)
 
     def InputFieldByID(self, id: str, value: str):
         """idを指定して入力バーに入力する"""
-        self.wait.until(EC.visibility_of_element_located((By.ID, id)))
-        elm = self.driver.find_element(by=By.ID, value=id)
+        self.web.wait.until(EC.visibility_of_element_located((By.ID, id)))
+        elm = self.web.driver.find_element(by=By.ID, value=id)
         self._InputField(elm, value)
 
     def InputFieldByName(self, name: str, value: str):
         """nameを指定して入力バーに入力する"""
-        self.wait.until(EC.visibility_of_element_located((By.NAME, name)))
-        elm = self.driver.find_element(by=By.NAME, value=name)
+        self.web.wait.until(EC.visibility_of_element_located((By.NAME, name)))
+        elm = self.web.driver.find_element(by=By.NAME, value=name)
         self._InputField(elm, value)
 
 
 def main():
-    driver = DownloadWebDriver()
+    chrome_wnd = ChromeWindow(WebDriverWindow(DownloadWebDriver()))
     url = "https://www.selenium.dev/selenium/web/web-form.html"
-    operator = DriverOperator(driver)
-    operator.Start(url)
-    operator.InputFieldByID("my-text-id", "Text input")
-    operator.InputFieldByName("my-password", "Password")
+    chrome_wnd.Start(url)
+    chrome_wnd.InputFieldByID("my-text-id", "Text input")
+    chrome_wnd.InputFieldByName("my-password", "Password")
     text_area_xpath = "/html/body/main/div/form/div/div[1]/label[3]/textarea"
-    operator.InputFieldByXPATH(text_area_xpath, "Textarea")
-    operator.Quit()
+    chrome_wnd.InputFieldByXPATH(text_area_xpath, "Textarea")
+    chrome_wnd.Quit()
 
 
 if __name__ == "__main__":
